f6a329c3d85adfa3b150d340c0ca3ce70b7d162e
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-04-23 13:45:31 -0300

    continuando funcoes

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+//sintaxe básica de declaração de função:
+function nomeDafuncao() {
+  //aqui vai o meu código
+}
+
+//exemplo 01
+
+function apresentacao() {
+  console.log("Olá, muito prazer, eu me chamo Rodrigo");
+}
+
+apresentacao();
+
+//exemplo 02
+
+const saudacao = function () {
+  console.log("Olá estamos aprendendo Javascript");
+};
+
+saudacao();
+
+//exemplo 03 (arrow function)
+
+const mensagem = () => {
+  console.log("Olá turma do GT, hoje estamos aprendendo funcoes");
+};
+
+mensagem();
