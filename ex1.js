96c08d92a2eb0b4f35826c6a233a2f7114c2a85e
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-04-23 15:41:23 -0300

    exercicios

---
+++ ex1.js
@@ -0,0 +1,6 @@
+let numero1 = prompt("Digite o primeiro numero:");
+let numero2 = prompt("Digite o segundo número:");
+
+let resultado = Number(numero1) + Number(numero2);
+
+console.log(resultado);
