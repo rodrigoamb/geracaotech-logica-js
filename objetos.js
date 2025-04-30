869682e1614b1f9af58be3299ca375da869cb9a4
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-04-30 14:05:57 -0300

    falando de objetos

--- objetos.js
+++ objetos.js
@@ -33,3 +33,50 @@ console.log(pessoa);
 delete pessoa.idade;
 
 console.log(pessoa);
+
+//desestruturação (destructuring) de objetos e arrays
+
+//em objetos
+const produto = {
+  nome: "notebook",
+  preco: 3500,
+  estoque: 20,
+};
+
+//em vez disso:
+
+// const nome = produto.nome
+// const preco = produto.preco
+// const estoque = produto.estoque
+
+//faça desestruturação:
+
+const { nome, preco, estoque } = produto;
+
+console.log(nome);
+console.log(preco);
+console.log(estoque);
+
+//Em arrays:
+
+let numeros = [10, 20, 30];
+
+//em vez disso:
+// const primeiro = numeros[0];
+// const segundo = numeros[1];
+
+//faça isso:
+const [primeiro, segundo] = numeros;
+
+console.log(primeiro);
+console.log(segundo);
+
+//com ...rest:
+
+let arrNumeros = [10, 20, 30, 40, 50];
+
+const [a, b, ...restoDoArray] = arrNumeros;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("resto do Array: ", restoDoArray);
